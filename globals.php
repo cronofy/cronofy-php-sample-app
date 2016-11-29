4bd92dcb0881cfde4a215acbd3e948dd5ab16001
Harry Boyes <harry.boyes@gmail.com>
2016-11-29 17:10:04 +0000

    Implement debuglog

--- globals.php
+++ globals.php
@@ -2,6 +2,10 @@
 
 include("lib/cronofy.php");
 
+function DebugLog($log){
+  $result = file_put_contents(__DIR__ . "/log/debug.log", date("c") . " - " . $log . "\n", FILE_APPEND);
+}
+
 $GLOBALS['CRONOFY_CLIENT_ID'] = "yptzUGc5Yq_oEWr-li2VZVjg9QE8EE2e";
 $GLOBALS['CRONOFY_CLIENT_SECRET'] = "0YqKTZjhnezSkRqL37J-n6JElt-TEPYdsM7u5MFlLaYJqqCbsOOOM1IczNu5DtnEbkRiOEAu20TtZWEKL8OHbA";
 
