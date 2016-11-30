0050645976e07b483b79ac6b7a6de1520fb1fe86
Harry Boyes <harry.boyes@gmail.com>
2016-11-30 16:17:20 +0000

    Event update validation

--- events/update.php
+++ events/update.php
@@ -2,6 +2,25 @@
 
 include("../config.php");
 
+$errors = [];
+
+if($_POST['event']['summary'] == ""){
+  array_push($errors, "errors[]=" . urlencode("Summary cannot be blank"));
+}
+if($_POST['event']['start'] == ""){
+  array_push($errors, "errors[]=" . urlencode("Start time cannot be blank"));
+}
+if($_POST['event']['end'] == ""){
+  array_push($errors, "errors[]=" . urlencode("End time cannot be blank"));
+}
+if($_POST['event']['start'] > $_POST['event']['end']){
+  array_push($errors, "errors[]=" . urlencode("Start time cannot be after end time"));
+}
+
+if(count($errors) > 0){
+  header('Location: ' . $GLOBALS['DOMAIN'] . '/events/edit.php?eventUid=' . $_POST['event']['event_uid'] . '&calendarId=' . $_POST['event']['calendar_id'] . '&' . join('&', $errors));
+}
+
 $cronofy->upsert_event(Array(
   "tzid" => "Etc/UTC",
   "event_id" => $_POST['event']['event_id'],
