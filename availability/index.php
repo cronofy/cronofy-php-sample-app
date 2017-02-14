46a84a4fc8671a4c8aa2d1aadac0f464c9e05215
Harry Boyes <harry.boyes@gmail.com>
2017-02-14 11:21:33 +0000

    Add default for account id in availability api

--- availability/index.php
+++ availability/index.php
@@ -86,7 +86,7 @@ include("../header.php"); ?>
       <div class="form-group">
         <label class="control-label col-lg-2">Account ID 1</label>
         <div class="col-lg-10">
-          <input class="form-control" type="text" name="availabilityInfo[accountId][0]" value="<?= $_POST['availabilityInfo']['accountId'][0] ?? "" ?>" />
+          <input class="form-control" type="text" name="availabilityInfo[accountId][0]" value="<?= $_POST['availabilityInfo']['accountId'][0] ?? $cronofy->get_account()["account"]["account_id"] ?>" />
           <span class="help-block">Click <a href="<?= $authUrl ?>" target="_blank">here</a> to find your Account ID</span>
         </div>
       </div>
