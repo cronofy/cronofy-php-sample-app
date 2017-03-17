40db40db59c49e7dc5e669b84b2407ac2264bd67
Harry Boyes <harry.boyes@gmail.com>
2017-03-17 13:09:46 +0000

    Use refreshable request and server error on availability

--- globals.php
+++ globals.php
@@ -6,10 +6,10 @@ function DebugLog($log){
   $result = file_put_contents(__DIR__ . "/log/debug.log", date("c") . " - " . $log . "\n", FILE_APPEND);
 }
 
-$GLOBALS['CRONOFY_CLIENT_ID'] = "";
-$GLOBALS['CRONOFY_CLIENT_SECRET'] = "";
+$GLOBALS['CRONOFY_CLIENT_ID'] = "kcmpdJJVDS8DtUwGOPlyGuSLQgLt4q6m";
+$GLOBALS['CRONOFY_CLIENT_SECRET'] = "vQbzdUNrv3VPsP69TELuuhkrV29U27yL7wbo0PiOvlSDVCMA_3yVavyXuk0-nYnAtX7zsEmyqBnTbRDN94DLTQ";
 
-$GLOBALS['DOMAIN'] = "";
+$GLOBALS['DOMAIN'] = "http://php.ngrok.io";
 
 if($GLOBALS['CRONOFY_CLIENT_ID'] == "" || $GLOBALS['CRONOFY_CLIENT_SECRET'] == "" || $GLOBALS['DOMAIN'] == ""){
   header('Location: /setup.php');
