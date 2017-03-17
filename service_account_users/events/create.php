c559a628ad70f7e559d4c1f68b4a5d7ed4747abb
Harry Boyes <harry.boyes@gmail.com>
2017-03-17 17:02:03 +0000

    Add server error to service account event creation

--- service_account_users/events/create.php
+++ service_account_users/events/create.php
@@ -24,7 +24,7 @@ if(count($errors) > 0){
   header('Location: ' . $GLOBALS['DOMAIN'] . '/events/new.php?calendarId=' . $_POST['event']['calendar_id'] . '&' . join('&', $errors));
 }
 
-$cronofy->upsert_event(Array(
+$event = Array(
   "tzid" => "Etc/UTC",
   "event_id" => $_POST['event']['event_id'],
   "calendar_id" => $_POST['event']['calendar_id'],
@@ -32,7 +32,17 @@ $cronofy->upsert_event(Array(
   "description" => $_POST['event']['description'],
   "start" => date('c', strtotime($_POST['event']['start'])),
   "end" => date('c', strtotime($_POST['event']['end'])),
-));
+);
+
+$result = CronofyRequest(function(){
+  return $GLOBALS['cronofy']->upsert_event($GLOBALS['event']);
+});
+
+if($result["error"]){
+  header('Location: ' . $GLOBALS['DOMAIN'] . 'service_account_users/events/new.php?calendarId=' . $_POST['event']['calendar_id'] . '&' . ErrorToQueryStringParams($result["error"]));
+
+  exit;
+}
 
 DebugLog("Service account create event success - event_id=`" . $_POST['event']['event_id'] . "` - calendar_id=`" . $_POST['event']['calendar_id'] . "` - summary=`" . $_POST['event']['summary'] . "` - description=`" . $_POST['event']['description'] . "` - start=`" . $_POST['event']['start'] . "` - end=`" . $_POST['event']['end'] . "`");
 
