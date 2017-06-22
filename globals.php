6bb2002ceee57348f24fc168f4220435193b8a1a
Stephen Binns <stephen.binns@gmail.com>
2017-06-22 10:10:44 +0100

    Use cronofy lib from packagist

--- globals.php
+++ globals.php
@@ -1,15 +1,14 @@
 <?php
-
-include("lib/cronofy.php");
+require 'vendor/autoload.php';
 
 function DebugLog($log){
   $result = file_put_contents(__DIR__ . "/log/debug.log", date("c") . " - " . $log . "\n", FILE_APPEND);
 }
 
-$GLOBALS['CRONOFY_CLIENT_ID'] = "kcmpdJJVDS8DtUwGOPlyGuSLQgLt4q6m";
-$GLOBALS['CRONOFY_CLIENT_SECRET'] = "vQbzdUNrv3VPsP69TELuuhkrV29U27yL7wbo0PiOvlSDVCMA_3yVavyXuk0-nYnAtX7zsEmyqBnTbRDN94DLTQ";
+$GLOBALS['CRONOFY_CLIENT_ID'] = "";
+$GLOBALS['CRONOFY_CLIENT_SECRET'] = "";
 
-$GLOBALS['DOMAIN'] = "http://php.ngrok.io";
+$GLOBALS['DOMAIN'] = "";
 
 if($GLOBALS['CRONOFY_CLIENT_ID'] == "" || $GLOBALS['CRONOFY_CLIENT_SECRET'] == "" || $GLOBALS['DOMAIN'] == ""){
   header('Location: /setup.php');
